067256d2bc408ef9d2e060c40348cf06316858c2
keroojoseph0 <keroojoseph0@gmail.com>
2026-06-16 14:03:30 +0300

    Custom the user model

--- api/models.py
+++ api/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+
+class CustomUser(AbstractUser):
+    email = models.EmailField(unique=True)
+    profile_picture_url = models.URLField(blank=True, null=True)
+
+
+    def __str__(self):
+        return self.email
+
+
--- project/settings.py
+++ project/settings.py
@@ -116,3 +116,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+
+
+AUTH_USER_MODEL = 'api.CustomUser'
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
