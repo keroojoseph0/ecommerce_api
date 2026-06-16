cd1bc36f3bdc1d862eb094f4fbe5b62e9b00efb6
keroojoseph0 <keroojoseph0@gmail.com>
2026-06-16 14:39:43 +0300

    Add related name in product category relationship

--- api/models.py
+++ api/models.py
@@ -32,7 +32,7 @@ class Product(models.Model):
     slug = models.SlugField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
     image = models.ImageField(upload_to='product_img', blank = True, null = True)
-    category = models.ForeignKey(Category, on_delete= models.SET_NULL, null = True , blank = True)
+    category = models.ForeignKey(Category, on_delete= models.SET_NULL, related_name = 'products', null = True , blank = True)
 
     def __str__(self):
         return self.name
