b5948476c349e33372d3fc2e506bf424a7851920
keroojoseph0 <keroojoseph0@gmail.com>
2026-06-16 19:24:48 +0300

    Managing the admin panel & populating the database

--- api/admin.py
+++ api/admin.py
@@ -1,16 +1,65 @@
 from django.contrib import admin
-from .models import Product, Category
+from django.contrib.auth.admin import UserAdmin
+from .models import Product, Category, CustomUser
 
 # Register your models here.
 
-admin.site.register(Product)
+
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    fieldsets = (
+        (
+            "Account Information",
+            {
+                "fields": (
+                    "username",
+                    "password",
+                )
+            },
+        ),
+        (
+            "Personal Information",
+            {
+                "fields": (
+                    "first_name",
+                    "last_name",
+                    "email",
+                )
+            },
+        ),
+        (
+            "Permissions",
+            {
+                "classes": ['collapse'],
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                )
+            },
+        ),
+        (
+            "Important Dates",
+            {
+                "classes": ['collapse'],
+                "fields": ('last_login', 'date_joined')
+            }
+        )
+    )
+
+
+
+
+@admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ['id', 'name', 'price']
+    list_display = ['id', 'name', 'slug', 'price']
     search_fields = ['name']
     list_filter = ['category']
 
 
-admin.site.register(Category)
+@admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ['id', 'name', 'slug']
     list_filter = ['name']
--- api/models.py
+++ api/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils.text import slugify
 from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
@@ -19,17 +20,32 @@ class Category(models.Model):
         verbose_name_plural = "Categories"
 
     name = models.CharField(max_length=100)
-    slug = models.SlugField(unique=True)
+    slug = models.SlugField(unique=True, blank=True)
     image = models.ImageField(upload_to='category_img', null = True, blank = True)
 
     def __str__(self):
         return self.name
+
+    def save(self, *args, **kwargs):
+
+        if not self.slug:
+            self.slug = slugify(self.name)
+            unique_slug = self.slug
+            counter = 1
+
+            while Category.objects.filter(slug=unique_slug).exclude(pk=self.pk).exists():
+                unique_slug = f"{self.slug}-{counter}"
+                counter += 1
+
+            self.slug = unique_slug
+
+        super().save(*args, **kwargs)
     
 
 class Product(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
-    slug = models.SlugField(unique=True)
+    slug = models.SlugField(unique=True, blank=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     image = models.ImageField(upload_to='product_img', blank = True, null = True)
     featured = models.BooleanField(default=True)
@@ -37,3 +53,18 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
+    
+    def save(self, *args, **kwargs):
+
+        if not self.slug:
+            self.slug = slugify(self.name)
+            unique_slug = self.slug
+            counter = 1
+
+            while Product.objects.filter(slug=unique_slug).exclude(pk=self.pk).exists():
+                unique_slug = f"{self.slug}-{counter}"
+                counter += 1
+
+            self.slug = unique_slug
+
+        super().save(*args, **kwargs)
